50de459ebf1da9e32aabd802dc191ba85ddb357d
ReyhanJanstephen <114643694+ReyhanJanstephen@users.noreply.github.com>
2022-09-30 22:21:14 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-
+   function startTime() {
+     const now = new Date();
+     let jam = now.getHours();
+     let menit = now.getMinutes();
+     let detik = now.getSeconds();
+     menit = checkTime(menit);
+     detik = checkTime(detik);
+     document.getElementById('clock').innerHTML = jam + ":" + menit + ":" + detik;
+     setTimeout(startTime, 1000);
+   }
+  
+   function checkTime(i){
+     if(i < 10){i = "0" + i};
+     return i;
+   }
