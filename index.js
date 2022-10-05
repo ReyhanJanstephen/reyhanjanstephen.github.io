6747b01820c7919ef0385e8ee723852b2e1df8e5
ReyhanJanstephen <114643694+ReyhanJanstephen@users.noreply.github.com>
2022-10-05 13:21:19 +0700

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@
   
    window.onclick = function(event) {
     if(!event.target.matches('.dropbtn')){
-       var dropdowns = document.getElementByClassName("dropdown-content");
+       var dropdowns = document.getElementsByClassName("dropdown-content");
        var i;
        for(i = 0; i < dropdowns.length; i++){
         var opendropdown = dropdowns[i];
