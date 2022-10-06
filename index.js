e910e656ef0f2744fc59141ed8661786c75ab28a
ReyhanJanstephen <114643694+ReyhanJanstephen@users.noreply.github.com>
2022-10-06 12:05:33 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
  var music1 = new Audio('ToBeContinuedFortnite.m4a');
  var music2 = new Audio('OneDirection.mp3');
+ var audio = document.querySelectorAll('Audio');
    function startTime() {
      const now = new Date();
      let jam = now.getHours();
@@ -20,11 +21,20 @@
      document.getElementById('dropdown1').classList.toggle("show");
 
    }
+
+   function stopallaudio(){
+     audio.forEach(function(audio){
+       audio.pause();
+       audio.currentTime = 0;
+     });
+   }
    function playmusic1(){
+     stopallaudio();
      music1.loop = true;
      music1.play();
    }
    function playmusic2(){
+     stopallaudio();
      music2.loop = true;
      music2.play();
    }
